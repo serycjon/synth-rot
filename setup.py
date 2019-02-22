b8723c0581a16c35e2c4dd943ffda3655796d0ee
serycjon <jonas.serych@gmail.com>
2019-02-22 13:58:26 +0100

    add: setuptools stuff

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='synth_rot',
+    version='0.1',
+    description='Synthetic tracking-by-segmentation dataset generator',
+    author='Jonas Serych',
+    author_email='jonas.serych@gmail.com',
+    packages=find_packages()
+    )
